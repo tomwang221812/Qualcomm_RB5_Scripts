3716eb5ff00698da13c8aae40d4ec3714c7db4e1
North Wang <tomwang221812@yahoo.com.tw>
2022-07-26 09:25:43 +0800

    AOSP Flash Images

---
+++ TVM_host/AOSP_flash_all.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+echo "(II) User Home Dir: $HOME"
+
+echo "(1/4) Start to flash ${PWD}/userdata.img"
+fastboot flash userdata userdata.img
+echo "(2/4) Start to flash ${PWD}/super.img"
+fastboot flash super super.img
+echo "(3/4) Start to flash ${PWD}/vendor_boot.img"
+fastboot flash vendor_boot vendor_boot.img
+echo "(4/4) Start to flash ${PWD}/boot.img"
+fastboot flash boot boot.img
+
+echo "(II) Try to reboot device"
+fastboot reboot
+
+echo "Wait for device connect... (Timeout 120 sec)"
+timeout 120 adb wait-for-device
