39682a75df1b2e094304a4e9e6205bb556de8ec6
North Wang <tomwang221812@yahoo.com.tw>
2022-07-26 10:42:32 +0800

    Build Thundercomm LU SDK

---
+++ TVM_host/UBUN_build.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+echo "Enter the sudo password: "
+read PW
+
+echo "Enter Thundercomm Email:"
+read TUMAIL
+echo "Enter Thundercomm Password:"
+read TUPASS
+
+echo "(II) User Home Dir: $HOME"
+export TOP_DIR = ${HOME}/Thundercomm
+echo "(II) Set TOP Dir: $TOP_DIR"
+export HOST_DIR = ${TOP_DIR}/SDK.turbox.rb5165-LU1.0-dev
+echo "(II) Set Host Dir: $HOST_DIR"
+
+echo "(!!) Stop and remove docker container that named ubuntu-18.04"
+echo $PW | sudo docker stop $(sudo docker ps -aqf "name=ubuntu-18.04")
+echo $PW | sudo docker rm $(sudo docker ps -aqf "name=ubuntu-18.04")
+echo $PW | sudo docker load < ubuntu-18.04.tar
+
+mkdir -P ${HOST_DIR}
+
+echo "(II) Run container (The path /home/user/host/ in container is mapped to host=> ${HOST_DIR})"
+echo $PW | sudo docker run -v ${HOST_DIR}:/home/user/host/ -d --name ubuntu-1804 –p 36000:22 ubuntu:18.04
+
+export CONPASS = "123456"
+echo "Connect to container by SSH, user password is 123456"
+echo "${CONPASS}" | ssh -o StrictHostKeyChecking=no -p 36000 user@127.0.0.1
+
+echo $PW | sudo apt update
+
+
