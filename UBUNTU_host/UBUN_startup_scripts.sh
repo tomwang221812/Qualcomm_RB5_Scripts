a7ea752bb7d8d2bd733565cba7317983aefa9eed
North Wang <tomwang221812@yahoo.com.tw>
2022-07-28 21:55:31 +0800

    Add ubuntu

--- UBUNTU_host/UBUN_startup_scripts.sh
+++ UBUNTU_host/UBUN_startup_scripts.sh
@@ -6,6 +6,7 @@ export SNPE_DIR='/home/tomwang/snpe-1.64.0.3605'
 adb devices
 
 adb shell "apt update && apt upgrade -y"
+adb shell "apt install -y git gcc iptables qt5-default qtwayland5 qtbase5-private-dev libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libgstreamer-plugins-bad1.0-dev gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-doc gstreamer1.0-tools gstreamer1.0-x gstreamer1.0-alsa gstreamer1.0-gl gstreamer1.0-gtk3 gstreamer1.0-qt5 gstreamer1.0-pulseaudio"
 adb shell "cp /lib/aarch64-linux-gnu/libc.so.6 /lib/libc.so.6"
 adb reboot
 timeout 120 adb wait-for-device
@@ -48,6 +49,19 @@ adb shell 'systemctl restart sshd'
 
 # Check Current IP
 adb shell 'ifconfig | grep inet'
+
+# Get sample-apps-for-Qualcomm-Robotics-RB5-platform from Github
+adb shell 'mkdir /data/sample-apps-for-Qualcomm-Robotics-RB5-platform && git clone https://github.com/quic/sample-apps-for-Qualcomm-Robotics-RB5-platform.git /data/sample-apps-for-Qualcomm-Robotics-RB5-platform'
+# Build RB5-Information(Device-info) Sample
+adb shell 'cd /data/sample-apps-for-Qualcomm-Robotics-RB5-platform/Device-info && mkdir bin && cd src && make && chmod +x ../bin/qrb5165_info && cp ../bin/qrb5165_info /usr/bin'
+adb shell qrb5165_info
+# Build RB5-Platform(GPIO-samples) Sample
+adb shell 'cd /data/sample-apps-for-Qualcomm-Robotics-RB5-platform/GPIO-samples && mkdir bin && cd src && make && chmod +x ../bin/qrb5165_platform && cp ../bin/qrb5165_platform /usr/bin'
+adb shell qrb5165_platform -led green 255
+# Build WIFI-On-Boarding Sample
+adb shell 'cd /data/sample-apps-for-Qualcomm-Robotics-RB5-platform/WIFI-OnBoarding/wifi && gcc wifi.c -o wifi && chmod +x wifi && cp wifi /usr/bin'
+# adb shell wifi
+
 
 echo "(!!) Done!"
 
